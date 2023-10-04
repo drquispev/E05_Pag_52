2313bce10194802776d6252d2de042941b546f35
drquispev <78872025+drquispev@users.noreply.github.com>
2023-10-04 17:23:55 -0500

    Add files via upload

--- E05.js
+++ E05.js
@@ -23,10 +23,54 @@ function Determine_Period() {
     }
 
     console.log(Array_X);
+    var Matrix_Print = [];
+    var ri = [];
+    let cont = 0;
+    for (let i = 0; i < Array_X.length; i++) {
+        cont++;
+        ri[i] = Array_X[i] / (m -1) ; // CALCULAR EL ri
+        Matrix_Print.push([cont,Array_X[i], ri[i]]);
+        console.log(cont,Array_X[i], ri[i], m-1);
+    }
+    return Array_X;
+}                                                                                 
+
+function Choose_Portion_Array(Array_X){
+    let aux = 101;
+    let aux2 = 200;
+    let j = 0;
+    var Portion_A = [];
+    for (let i= aux; i<aux2; i++){
+        Portion_A[j] = Array_X[i];
+        j++;
+    }
+    console.log(`Portion_A: ${Portion_A}`);
+    return Portion_A;
 }
 
+function Prueba_Media(Array_X){
+    let r_raya = 0;
+    let suma_ri = 0;
+    for ( let i=0; i<Array_X.length; i++){
+        suma_ri = Array_X[i] = suma_ri;
+    }
+    r_raya = suma_ri / Array_X.length;
+
+    aux
+}   
+
+function Prueba_Varianza(Array_X){
+
+}
+
+function Prueba_Poker(Array_X){
+
+}
 const btn = document.getElementById('button_calculate');
     btn.addEventListener('click', () => {
         Determine_Period();
+        var Array = Determine_Period();
+        Choose_Portion_Array(Array);
+
         //Print_Table(valorSeleccionado);
-    }); 
+}); 
